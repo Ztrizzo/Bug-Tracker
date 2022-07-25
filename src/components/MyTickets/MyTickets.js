433f1df706e47f1b37f52c28375765d18e7c8342
Zach Rizzo <RizzoZachary2@gmail.com>
2022-07-24 19:19:01 -0500

    created route to see logged in users tickets

--- src/components/MyTickets/MyTickets.js
+++ src/components/MyTickets/MyTickets.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 
-export default function MyTickets(){
+export default function MyTickets({ tickets, user }){
   return(
-    <h1>MY TICKETS</h1>
+    <div>
+      <h1>{user.name}'s Tickets</h1>
+
+      {tickets.map(ticket => {
+        return(
+          <div key={ticket.id}>
+            <Link to={`/api/tickets/${ticket.id}`}>{ticket.title}</Link>
+          </div>
+
+        )
+      })}
+    </div>
   )
 }
