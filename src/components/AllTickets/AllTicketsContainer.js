d779c19e9d09ce0c2a39aed60954630a92b6070d
Zach Rizzo <RizzoZachary2@gmail.com>
2022-08-05 16:42:24 -0500

    adding grid display to individuals tickets

--- src/components/AllTickets/AllTicketsContainer.js
+++ src/components/AllTickets/AllTicketsContainer.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import AllTickets from "./AllTickets";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function AllTicketsContainer(){
   const [allTickets, setAllTickets] = useState([]);
-  
+  const navigate = useNavigate();
+
   useEffect(() => {
     axios.get('/api/tickets')
     .then(
@@ -12,7 +14,11 @@ export default function AllTicketsContainer(){
     )
   }, [])
 
+  const handleOnCellClick = ({ id }) =>{
+    navigate(`/tickets/${id}`);
+  }
+  
   return(
-    <AllTickets allTickets={allTickets}/>
+    <AllTickets allTickets={allTickets} handleOnCellClick={handleOnCellClick}/>
   )
 }
